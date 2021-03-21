6974f2c3382bf57a08a8a8aeebd86a276bb9f772
serialmonk <itsdeveloperdeep@gmail.com>
2021-03-21 21:55:50 +0530

    add backend support

--- components/Hero.vue
+++ components/Hero.vue
@@ -4,8 +4,8 @@
         <div class="container mx-auto mb-20 min-h-screen relative">
             <Nav/>
             <div class="flex flex-col justify-center mt-20 h-96 items-center">
-                <h1 class="text-5xl text-white font-semibold lg:px-20 md:px-12 text-center my-5">One Click & Add Dynamic Data Into Your Website .</h1>
-                <p class="text-gray-600 lg:mx-64 md:mx-12 text-center">Quickly edit data and add dynamic data into your website. No installation, no database to set up, and is completely secure. Its free :)</p>
+                <h1 class="text-5xl text-white font-semibold lg:px-20 md:px-12 text-center my-5">Convert Your Static Website Into Dynamic One <br> with just one click</h1>
+                <p class="text-gray-600 lg:mx-64 md:mx-12 text-center">Quickly edit data and add dynamic data into your website. <br> No installation, no database to set up, and is completely secure. Its free 🔥</p>
                 <a class="inline-block bg-lightBlue uppercase text-white text-md font-semibold px-20 py-3 rounded-lg mt-10" href="#!">Try Demo</a>
                 <p class="text-gray-700 mt-4">No credit card required</p>
             </div>
--- nuxt.config.js
+++ nuxt.config.js
@@ -21,6 +21,7 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/vue-clipboard.js',
     '~/plugins/vue-tooltip.js'
   ],
 
